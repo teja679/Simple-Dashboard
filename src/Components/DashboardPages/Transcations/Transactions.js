d98f93237d3c6ce01bd64ad3674b4d36cd99c4aa
Teja <lovlytejaa@gamil.com>
2022-10-26 16:59:45 +0530

    transactions

---
+++ src/Components/DashboardPages/Transcations/Transactions.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './styles.css'
+import ErrorIcon from '@mui/icons-material/Error';
+
+const Transactions = () => {
+
+    const data = [
+        {
+            id: 1,
+            name: 'Super Packaging',
+            date: 'October 26, 2022',
+            customer: 'Marsha Jola',
+            price: '$250,39',
+            status: 'recieved',
+        },
+        {
+            id: 2,
+            name: 'Super Packaging',
+            date: 'October 26, 2022',
+            customer: 'Marsha Jola',
+            price: '$250,39',
+            status: 'recieved',
+        },
+        {
+            id: 3,
+            name: 'Super Packaging',
+            date: 'October 26, 2022',
+            customer: 'Marsha Jola',
+            price: '$250,39',
+            status: 'recieved',
+        },
+    ]
+  return (
+    <div className='transactions'>
+        <div className='header'>
+            <h1>Last Transaction</h1>
+            <ErrorIcon />
+        </div>
+        <table>
+            <tr className='trow'>
+                <th>No</th>
+                <th>Product Name</th>
+                <th>Date</th>
+                <th>Customers</th>
+                <th>Price</th>
+                <th>Status Orders</th>
+                <th>Action</th>
+            </tr>
+            {data.map((item, index) => (
+                <tr key={item.id} className='trow'>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.date}</td>
+                    <td>{item.customer}</td>
+                    <td>{item.price}</td>
+                    <td>{item.status}</td>
+                    <td>...</td>
+                </tr>
+            ))}
+        </table>
+    </div>
+  )
+}
+
+export default Transactions
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './styles.css'
 import BarChart from '../../Components/DashboardPages/BarChart/BarChart'
 import Search from '../../Components/DashboardPages/Intro/Search'
+import Transactions from '../../Components/DashboardPages/Transcations/Transactions'
 
 const Dashboard = () => {
   return (
     <div className='dashboard-div'>
         <Search />
         <BarChart />
+        <Transactions />
     </div>
   )
 }
